3db45a69611424696ad7d8d49e91dc82955385ea
francepants <franceline90@gmail.com>
2020-05-30 16:15:28 -0500

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,22 @@
 require 'pry'
 
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     binding.pry
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
 def hello(array)
   i = 0
+  collection = [] #empty array that will have the return values frm line 16
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i]) #pushed the return value to the empty array
     i += 1
   end
+  collection #need to return the new array at the end of the method
 end
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name| "Hi, #{name}" }
