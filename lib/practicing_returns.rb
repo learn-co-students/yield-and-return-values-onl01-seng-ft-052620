c96adc734c130c2cd8732eec705a83a7a8b55a52
Julie Garcia <juliegarcia.cali@gmail.com>
2020-06-02 19:55:57 +0000

    done

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,15 @@
 require 'pry'
-
-def hello(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i += 1
+def hello_t(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length
+      yield(array[i])
+      i = i + 1
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
   end
 end
-
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
